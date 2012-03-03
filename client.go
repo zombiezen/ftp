049c9671c7a562980b04ae081cf74f4c1f94be8a
Ross Light <ross@zombiezen.com>
2012-03-03 10:18:58 -0800

    Refactor client

--- client.go
+++ client.go
@@ -12,7 +12,8 @@ import (
 	"strings"
 )
 
-// A Client is an FTP client.
+// A Client is an FTP client.  A single FTP connection cannot handle
+// simultaneous transfers.
 type Client struct {
 	c       net.Conn
 	proto   *textproto.Conn
@@ -39,13 +40,13 @@ func NewClient(c net.Conn) (*Client, error) {
 	if err != nil {
 		return nil, err
 	}
+
 	return client, nil
 }
 
 // Quit sends the QUIT command and closes the connection.
 func (client *Client) Quit() error {
-	_, err := client.Do("QUIT")
-	if err != nil {
+	if _, err := client.sendCommand("QUIT"); err != nil {
 		return err
 	}
 	return client.Close()
@@ -58,12 +59,12 @@ func (client *Client) Close() error {
 
 // Login sends credentials to the server.
 func (client *Client) Login(username, password string) error {
-	reply, err := client.Do("USER " + username)
+	reply, err := client.sendCommand("USER " + username)
 	if err != nil {
 		return err
 	}
 	if reply.Code == CodeNeedPassword {
-		reply, err = client.Do("PASS " + password)
+		reply, err = client.sendCommand("PASS " + password)
 		if err != nil {
 			return err
 		}
@@ -74,67 +75,17 @@ func (client *Client) Login(username, password string) error {
 	return nil
 }
 
-// response reads a reply from the server.
-func (client *Client) response() (Reply, error) {
-	line, err := client.proto.ReadLine()
-	if err != nil {
-		return Reply{}, err
-	} else if len(line) < 4 {
-		return Reply{}, errors.New("Short response line in FTP")
-	}
-
-	code, err := strconv.Atoi(line[:3])
-	if err != nil {
-		return Reply{}, err
-	}
-
-	reply := Reply{Code: Code(code)}
-	switch line[3] {
-	case '-':
-		lines := []string{line[4:]}
-		endPrefix := strconv.Itoa(code) + " "
-		for {
-			line, err = client.proto.ReadLine()
-			if err != nil {
-				break
-			}
-			if strings.HasPrefix(line, endPrefix) {
-				lines = append(lines, line[len(endPrefix):])
-				break
-			} else {
-				lines = append(lines, line)
-			}
-		}
-		reply.Msg = strings.Join(lines, "\n")
-		return reply, err
-	case ' ':
-		reply.Msg = line[4:]
-	default:
-		return Reply{}, errors.New("Expected space after FTP response code")
-	}
-	return reply, nil
-}
-
 // Do sends a command over the control connection and waits for the response.  It returns any
 // protocol error encountered while performing the command.
 func (client *Client) Do(command string) (Reply, error) {
-	err := client.proto.PrintfLine("%s", command)
-	if err != nil {
-		return Reply{}, err
-	}
-	r, err := client.response()
-	return r, err
+	return client.sendCommand(command)
 }
 
-// Passive opens a new passive data port.
-func (client *Client) Passive() (io.ReadWriteCloser, error) {
-	var reply Reply
-	var addr *net.TCPAddr
-	var err error
-
-	switch client.c.RemoteAddr().Network() {
-	case "tcp6":
-		reply, err = client.Do("EPSV")
+// obtainPassiveAddress returns the address to dial for a new passive data
+// connection.
+func (client *Client) obtainPassiveAddress() (*net.TCPAddr, error) {
+	if client.c.RemoteAddr().Network() == "tcp6" {
+		reply, err := client.sendCommand("EPSV")
 		if err != nil {
 			return nil, err
 		} else if reply.Code != CodeExtendedPassive {
@@ -146,24 +97,27 @@ func (client *Client) Passive() (io.ReadWriteCloser, error) {
 			return nil, err
 		}
 
-		addr = &net.TCPAddr{
+		return &net.TCPAddr{
 			IP:   client.c.RemoteAddr().(*net.TCPAddr).IP,
 			Port: port,
-		}
-	default:
-		reply, err = client.Do("PASV")
-		if err != nil {
-			return nil, err
-		} else if reply.Code != CodePassive {
-			return nil, reply
-		}
-
-		addr, err = parsePasvReply(reply.Msg)
-		if err != nil {
-			return nil, err
-		}
+		}, nil
 	}
 
+	reply, err := client.sendCommand("PASV")
+	if err != nil {
+		return nil, err
+	} else if reply.Code != CodePassive {
+		return nil, reply
+	}
+	return parsePasvReply(reply.Msg)
+}
+
+// openPassive creates a new passive data connection.
+func (client *Client) openPassive() (*net.TCPConn, error) {
+	addr, err := client.obtainPassiveAddress()
+	if err != nil {
+		return nil, err
+	}
 	return net.DialTCP("tcp", nil, addr)
 }
 
@@ -211,12 +165,11 @@ type transferConn struct {
 }
 
 func (conn transferConn) Close() error {
-	err := conn.ReadWriteCloser.Close()
-	if err != nil {
+	if err := conn.ReadWriteCloser.Close(); err != nil {
 		return err
 	}
-	reply, err := conn.client.response()
-	if err != nil {
+
+	if reply, err := conn.client.response(); err != nil {
 		return err
 	} else if !reply.PositiveComplete() {
 		return reply
@@ -225,33 +178,31 @@ func (conn transferConn) Close() error {
 }
 
 // transfer sends a command and opens a new passive data connection.
-func (client *Client) transfer(command, dataType string) (io.ReadWriteCloser, error) {
+func (client *Client) transfer(command, dataType string) (conn io.ReadWriteCloser, err error) {
 	// Set type
-	reply, err := client.Do("TYPE " + dataType)
-	switch {
-	case err != nil:
+	if reply, err := client.sendCommand("TYPE " + dataType); err != nil {
 		return nil, err
-	case !reply.PositiveComplete():
+	} else if !reply.PositiveComplete() {
 		return nil, reply
 	}
 
 	// Open data connection
-	conn, err := client.Passive()
+	conn, err = client.openPassive()
 	if err != nil {
 		return nil, err
 	}
+	defer func(conn io.Closer) {
+		if err != nil {
+			conn.Close()
+		}
+	}(conn)
 
 	// Send command
-	reply, err = client.Do(command)
-	switch {
-	case err != nil:
-		conn.Close()
+	if reply, err := client.sendCommand(command); err != nil {
 		return nil, err
-	case !reply.Positive():
-		conn.Close()
+	} else if !reply.Positive() {
 		return nil, reply
 	}
-
 	return transferConn{conn, client}, nil
 }
 
@@ -263,4 +214,53 @@ func (client *Client) Text(command string) (io.ReadWriteCloser, error) {
 // Binary sends a command and opens a new passive data connection in image mode.
 func (client *Client) Binary(command string) (io.ReadWriteCloser, error) {
 	return client.transfer(command, "I")
+}
+
+func (client *Client) sendCommand(command string) (Reply, error) {
+	err := client.proto.PrintfLine("%s", command)
+	if err != nil {
+		return Reply{}, err
+	}
+	return client.response()
+}
+
+// response reads a reply from the server.
+func (client *Client) response() (Reply, error) {
+	line, err := client.proto.ReadLine()
+	if err != nil {
+		return Reply{}, err
+	} else if len(line) < 4 {
+		return Reply{}, errors.New("Short response line in FTP")
+	}
+
+	code, err := strconv.Atoi(line[:3])
+	if err != nil {
+		return Reply{}, err
+	}
+
+	reply := Reply{Code: Code(code)}
+	switch line[3] {
+	case '-':
+		lines := []string{line[4:]}
+		endPrefix := strconv.Itoa(code) + " "
+		for {
+			line, err = client.proto.ReadLine()
+			if err != nil {
+				break
+			}
+			if strings.HasPrefix(line, endPrefix) {
+				lines = append(lines, line[len(endPrefix):])
+				break
+			} else {
+				lines = append(lines, line)
+			}
+		}
+		reply.Msg = strings.Join(lines, "\n")
+		return reply, err
+	case ' ':
+		reply.Msg = line[4:]
+	default:
+		return Reply{}, errors.New("Expected space after FTP response code")
+	}
+	return reply, nil
 }
